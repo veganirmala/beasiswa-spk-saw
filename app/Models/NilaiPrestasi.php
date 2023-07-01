8671920d4148778a308389b5aef7134c5f3e08cd
sagetamerta <38368716+sagetamerta@users.noreply.github.com>
2023-07-02 01:55:46 +0800

    fixing others naming Models

--- app/Models/NilaiPrestasi.php
+++ app/Models/NilaiPrestasi.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Nilaiprestasi extends Model
+class NilaiPrestasi extends Model
 {
     use HasFactory;
 
