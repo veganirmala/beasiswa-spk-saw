84d681cee26f991c74d316f55c8a6a0922097e4b
sagetamerta <38368716+sagetamerta@users.noreply.github.com>
2023-07-08 14:29:36 +0800

    up foreign key edit on ipk and prodi

--- resources/views/ipk/show.blade.php
+++ resources/views/ipk/show.blade.php
@@ -9,20 +9,20 @@
         <div class="container-fluid">
             <h3>Detail Data IPK</h3>
             <form action="/ipk/{{ $ipk->id }}" method="POST">
-                 @csrf
-            <div class="card mb-3 col-lg-8" style="max-width: 540px;">
-                <div class="row no-gutters">
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h4 class="card-title">NIM</h4>
-                            <p class="card-text"><?= $ipk['nim']; ?></p>
-                            <h4 class="card-title">Nilai IPK</h4>
-                            <p class="card-text"><?= $ipk['nilai_ipk']; ?></p>
+                @csrf
+                <div class="card mb-3 col-lg-8" style="max-width: 540px;">
+                    <div class="row no-gutters">
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h4 class="card-title">NIM</h4>
+                                <p class="card-text"><?= $ipk['nim'] ?></p>
+                                <h4 class="card-title">Nilai IPK</h4>
+                                <p class="card-text"><?= $ipk['nilai_ipk'] ?></p>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </form>
+            </form>
             <button type="button" value="Kembali" onClick="history.go(-1)" class="btn btn-primary btn-user">
                 Kembali
             </button>
